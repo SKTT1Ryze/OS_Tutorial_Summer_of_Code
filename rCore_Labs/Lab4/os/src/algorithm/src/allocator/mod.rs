06ad82add4f98500ed4ba2bb5efded26639f0175
hustccc <1276675421@qq.com>
2020-07-20 23:24:49 +0900

    add BuddySystemAllocator

--- rCore_Labs/Lab4/os/src/algorithm/src/allocator/mod.rs
+++ rCore_Labs/Lab4/os/src/algorithm/src/allocator/mod.rs
@@ -4,13 +4,14 @@ mod segment_tree_allocator;
 mod stacked_allocator;
 mod free_list_allocator;
 mod bitmap_vector_allocator;
+mod buddy_system_vector_allocator;
 /// 分配器：固定容量，每次分配 / 回收一个元素
 pub trait Allocator {
-    /// 给定容量，创建分配器
+    /// create allocator with capacity
     fn new(capacity: usize) -> Self;
-    /// 分配一个元素，无法分配则返回 `None`
+    /// alloc a item. error return `None`
     fn alloc(&mut self) -> Option<usize>;
-    /// 回收一个元素
+    /// dealloc a item
     fn dealloc(&mut self, index: usize);
 }
 
@@ -23,16 +24,18 @@ pub use free_list_allocator::FreeListAllocator;
 // pub type AllocatorImpl = SegmentTreeAllocator;
 pub type AllocatorImpl = StackedAllocator;
 
+/// 分配器：固定容量，每次分配 / 回收指定大小的元素
 pub trait VectorAllocator {
-    /// 给定容量，创建分配器
+    /// create allocator with capacity
     fn new(capacity: usize) -> Self;
-    /// 分配指定长度的空间，无法分配则返回 `None`
+    /// alloc space with size of `size`. error return `None`
     fn alloc(&mut self, size: usize, align: usize) -> Option<usize>;
-    /// 回收指定空间（一定是之前分配的）
+    /// dealloc space with `start` and `size`
     fn dealloc(&mut self, start: usize, size: usize, align: usize);
 }
 
 pub use bitmap_vector_allocator::BitmapVectorAllocator;
-
+pub use buddy_system_vector_allocator::BuddySystemVectorAllocator;
 /// default VectorAllocator
-pub type VectorAllocatorImpl = BitmapVectorAllocator;
+//pub type VectorAllocatorImpl = BitmapVectorAllocator;
+pub type VectorAllocatorImpl = BuddySystemVectorAllocator;
